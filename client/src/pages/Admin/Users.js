81354d4d0393536ca30a3be1547601ac3fc2df44
humblex <praneeykauri@gmail.com>
2023-05-08 22:27:35 +0545

    yoy

--- client/src/pages/Admin/Users.js
+++ client/src/pages/Admin/Users.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AdminMenu from "../../components/Layout/AdminMenu";
 import Layout from "./../../components/Layout/Layout";
+import Box  from "@mui/material/Box";
 
 const Users = () => {
   return (
     <Layout title={"Dashboard - All Users"}>
+      <Box height={45} />
       <div className="container-fluid m-3 p-3">
         <div className="row">
           <div className="col-md-3">
--- client/src/pages/Vendor/VendorUsers.js
+++ client/src/pages/Vendor/VendorUsers.js
@@ -1,10 +1,12 @@
 import React from "react";
 import VendorMenu from "../../components/Layout/VendorMenu";
 import Layout from "./../../components/Layout/Layout";
+import Box  from "@mui/material/Box";
 
 const VendorUsers = () => {
   return (
     <Layout title={"Dashboard - All Users"}>
+      <Box height={45} />
       <div className="container-fluid m-3 p-3">
         <div className="row">
           <div className="col-md-3">
